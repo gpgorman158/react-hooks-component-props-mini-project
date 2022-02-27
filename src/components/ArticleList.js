e1c5c5ed3448f421740c013ed42568c474fad5cb
Gerald Gorman <gpgorman158@gmail.com>
2022-02-27 17:11:24 -0500

    commit tree 872c3bfdb2d3942caba53b5acc3b11ac94378a3d

---
+++ src/components/ArticleList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Article from "./Article.js";
+
+function ArticleList (props) {
+
+    return (
+        <main>
+            {props.posts.map((post) => {
+                return (
+                    <Article 
+                        key={post.id}
+                        title={post.title}
+                        date={post.date}
+                        preview={post.preview}
+                    />
+                )}
+            )}
+            
+        </main>
+    )
+
+}
+
+export default ArticleList;
